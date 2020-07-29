ef55288a08dbef351385361d13a22ff7aca475a3
huangal5 <alisa.y.s.huang@oocl.com>
2020-07-29 17:47:20 +0800

    [JPA] [Yancy & Alisa] feature : add contribute in entity

--- src/main/java/com/thoughtworks/springbootemployee/entity/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/entity/Employee.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.springbootemployee.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private Company company;
+
+
 }
--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -1,4 +1,8 @@
 package com.thoughtworks.springbootemployee.service;
 
+import com.thoughtworks.springbootemployee.entity.Employee;
+
 public interface EmployeeService {
+
+
 }
