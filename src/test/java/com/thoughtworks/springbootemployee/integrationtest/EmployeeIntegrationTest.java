eccac49a8944f36086396b13696a84541a8c0ab0
huangal5 <alisa.y.s.huang@oocl.com>
2020-08-01 18:42:23 +0800

    [JPA] [Yancy & Alisa] feature: pass the integrationTest case of findEmployeeById

---
+++ src/test/java/com/thoughtworks/springbootemployee/integrationtest/EmployeeIntegrationTest.java
@@ -0,0 +1,53 @@
+package com.thoughtworks.springbootemployee.integrationtest;
+
+
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @AfterEach
+    private void cleanUp(){
+        employeeRepository.deleteAll();
+        companyRepository.deleteAll();
+    }
+
+    @Test
+    void should_return_employee_when_find_employee_by_id_given_employee_id() throws Exception {
+        Employee employee = new Employee();
+        employee.setName("Yancy");
+        Company company = new Company();
+        company.setName("tw");
+        companyRepository.save(company);
+        employee.setCompany(company);
+        int employeeId = employeeRepository.save(employee).getId();
+
+        mockMvc.perform(get("/employees/" + employeeId)).andExpect(status().isOk());
+    }
+
+
+}
