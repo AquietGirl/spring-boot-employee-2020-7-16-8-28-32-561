2b4b8ad595ab5f5e8a69504973a3ad61145501fd
huangal5 <alisa.y.s.huang@oocl.com>
2020-08-02 16:19:33 +0800

    [JPA] [Yancy & Alisa] refactor: pass the integrationTest case of should_return_company_when_find_company_by_id_given_company_id

--- src/test/java/com/thoughtworks/springbootemployee/integrationtest/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationtest/EmployeeIntegrationTest.java
@@ -7,17 +7,13 @@ import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.awt.print.Pageable;
-
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
